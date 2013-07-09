79398e903fd62f33a5f3bf4c52fbef59d60da7b6
Rich Tanner <rwtanner@iastate.edu>
2013-07-08 22:14:02 -0500

    CSS changes
    Added fixed height div for header extras, and adjusted other pretties

--- includes/constants/header.inc.php
+++ includes/constants/header.inc.php
@@ -4,36 +4,38 @@
 <!-- add link to jquery library -->
 <script src="includes/js/jquery-1.9.1.min.js"></script>  
 
-<script>
-$ (function() //function that gets called whenever the document is loaded
-{
-	//what happens when mousing over the button?
-	// $ ("#logOff_button").mouseenter(function() {
-	// 	$(this).css("background-color","#000000");
-	// });
-	
-	// $ ("#logOff_button").mouseleave(function() {
-	// 	$(this).css("background-color","#FFFFFF");
-	// });
-	
-	//Detect click on area
-	$("#logOff_button").click(function() {
-		// call majic php to clear our logon session
-		$.get("clearsession.php");
+<div id="divHeaderPanel">
+	<script>
+	$ (function() //function that gets called whenever the document is loaded
+	{
+		//what happens when mousing over the button?
+		// $ ("#logOff_button").mouseenter(function() {
+		// 	$(this).css("background-color","#000000");
+		// });
+		
+		// $ ("#logOff_button").mouseleave(function() {
+		// 	$(this).css("background-color","#FFFFFF");
+		// });
+		
+		//Detect click on area
+		$("#logOff_button").click(function() {
+			// call majic php to clear our logon session
+			$.get("clearsession.php");
 
-		// redirect, now that we are logged off
-		// If we redirect too soon, we lose our php post, so set a 1 second timer before redirect
-		setTimeout('window.location.href = "http://localhost:8888/HCI7573-FinalProject/HCI%20573/index.php"', 1000);
+			// redirect, now that we are logged off
+			// If we redirect too soon, we lose our php post, so set a 1 second timer before redirect
+			setTimeout('window.location.href = "http://localhost:8888/HCI7573-FinalProject/HCI%20573/index.php"', 1000);
+		});
 	});
-});
-</script>
+	</script>
 
-<?php 
-	if ($_SESSION['userName'] != '') {
-		echo "Welcome to Sustainable Journey, " . $_SESSION['userName'] . ". ";
-		echo "<button type=\"logOff\" class=\"logOff\" id=\"logOff_button\">Log Off</button>";
-	} else {
-		echo "You are not logged in";
-	}
-?>
+	<?php 
+		if ($_SESSION['userName'] != '') {
+			echo "Welcome to Sustainable Journey, " . $_SESSION['userName'] . ". ";
+			echo "<button type=\"logOff\" class=\"logOff\" id=\"logOff_button\">Log Off</button>";
+		} else {
+			echo "You are not logged in";
+		}
+	?>
+</div>
 	
